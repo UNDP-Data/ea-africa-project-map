d8160cbc70aa4dad85911b52ba3ea875d3b98d8b
data-undp <106840132+data-undp@users.noreply.github.com>
2022-07-25 20:12:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -200,7 +200,7 @@ const App = () => {
       <SubNote>
         *each circle represent 5 MilLion People
       </SubNote>
-      <SubTitle>Targeted African countries</SubTitle>
+      <SubTitle>Targeted countries</SubTitle>
       <UnivariateMap selectedValue={value} />
     </>
   );
