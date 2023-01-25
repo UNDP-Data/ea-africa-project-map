9146126919acf796ce7f19c79ed24f0ab86ea68a
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2023-01-25 14:21:57 +0200

    Fix tooltip

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -16,8 +16,9 @@ const TooltipEl = styled.div<TooltipElProps>`
   display: block;
   position: fixed;
   z-index: 5;
-  background-color: var(--gray-200);
-  padding: 0.5rem 2rem;
+  background-color: var(--gray-100);
+  border: 1px solid var(--gray-400);
+  padding: 0.5rem var(--spacing-05);
   word-wrap: break-word;
   top: ${(props) => (props.verticalAlignment === 'bottom' ? props.y - 40 : props.y + 40)}px;
   left: ${(props) => (props.horizontalAlignment === 'left' ? props.x - 20 : props.x + 20)}px;
@@ -31,9 +32,9 @@ export const Tooltip = (props: Props) => {
   } = props;
   return (
     <TooltipEl x={data.xPosition} y={data.yPosition} verticalAlignment={data.yPosition > window.innerHeight / 2 ? 'top' : 'bottom'} horizontalAlignment={data.xPosition > window.innerWidth / 2 ? 'left' : 'right'}>
-      <h3 className='bold'>
+      <h2 className='bold margin-top-00 margin-bottom-05'>
         {data.country}
-      </h3>
+      </h2>
       {
         !data.AMP && !data.AO ? (
           <>
@@ -44,16 +45,14 @@ export const Tooltip = (props: Props) => {
         ) : (
           <>
             <p className='undp-typography'>
-              <div>
-                Part of:
-                {' '}
-                <span className='bold'>{data.AMP && data.AO ? 'AO & AMP both' : data.AMP ? 'AMP Only' : 'AO Only'}</span>
-              </div>
-              <div>
-                Potential Beneficiaries:
-                {' '}
-                <span className='bold'>{format('0.2s')(data.value)}</span>
-              </div>
+              Part of:
+              {' '}
+              <span className='bold'>{data.AMP && data.AO ? 'AO & AMP both' : data.AMP ? 'AMP Only' : 'AO Only'}</span>
+            </p>
+            <p className='undp-typography'>
+              Potential Beneficiaries:
+              {' '}
+              <span className='bold'>{format('0.2s')(data.value)}</span>
             </p>
           </>
         )
